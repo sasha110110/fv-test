9ddc3001b018ce9d6706f5b1f6da67d44c883de7
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-13 23:10:35 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,6 +23,29 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook_handler():
+   # global updater
+    
+    if request.method == "POST":
+        global content #WORKING
+        content = json.loads(request.get_data())# #WORKING
+
+        
+        print(content)
+        chat_id= msg.sender_chat["username"]
+        bot.sendMessage(chat_id=update.message.chat_id, text=str(content))
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    bot.deleteWebhook()
+    time.sleep(2)
+    s = bot.setWebhook(f"https://fv-test.vercel.app/{TOKEN}") #WORKING without token 
+    #s=requests.get(f'https://api.telegram.org/bot{TOKEN}/setWebHook?url={HOST}/{TOKEN}>&allowed_updates=["callback_query","message"]') #without token
+    
+    if s:
+        return str(bot.getWebhookInfo())
+    else:
+        return "webhook setup failed"
