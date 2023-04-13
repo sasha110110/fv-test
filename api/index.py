87d5e0617b658b3f2e2eec10d45dd9c47e399dec
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-13 19:21:40 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import json
+import telegram
+from telegram.ext import Updater , CommandHandler , Filters , MessageHandler
+import time
+
+
+TOKEN = "5858379831:AAGLpbVbx0TRGk5ctcch3dKvcOx4JrmBhuA"
+HOST="https://fv-test.vercel.app"
+global bot
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+#bot.deleteWebhook()"
+HOST="https://tg-bot-test-2.vercel.app"
+global bot
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+#bot.deleteWebhook()
 
 app = Flask(__name__)
 
