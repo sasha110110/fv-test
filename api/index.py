4d0a710cb8b829fe3b5920381eb8c20fae8f601a
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-04-14 20:10:17 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,12 +10,6 @@ HOST="https://fv-test.vercel.app"
 bot = telegram.Bot(token=TOKEN)
 updater = Updater(token=TOKEN, use_context=True)
 
-
-def greet(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there")
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,24 +17,32 @@ def home():
     return 'Hello, World!'
 
 @app.route(f'/{TOKEN}', methods=['POST'])
-def webhook_handler(pdate, context):
+def webhook_handler():
+    global bot
    # global updater
     
-    ##if request.method == "POST":
+    if request.method == "POST":
         #global content #WORKING
-        #content=request.json
         #content = json.loads(request.get_data())# #WORKING
        
         #chat_id = request.json["message"]["chat"]["id"]
-        print(content)
-    chat_id="1093497662"# msg.sender_chat["username"]
-    update = telegram.update.Update.de_json(request.get_json(force=True))
-    bot.sendMessage(chat_id=chat_id, text=str(update))
+    #chat_id="1093497662"# msg.sender_chat["username"]
+   
+        msg = telegram.Update.de_json(request.get_json(force=True))
+
+        chat_id = msg.message.chat.id
+
+        text = msg.message.text.encode('utf-8')
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
     return 'ok'
 
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
+    global bot
     bot.deleteWebhook()
     time.sleep(2)
     s = bot.setWebhook(f"https://fv-test.vercel.app/{TOKEN}") #WORKING without token 
